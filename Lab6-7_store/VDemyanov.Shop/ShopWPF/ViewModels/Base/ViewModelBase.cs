5c69afad6bb7b3ab28b34f09902404d8236da2be
Vlad Demyanov <vlad.demyanov.14steam@mail.ru>
2021-03-28 15:22:11 +0300

    Lab6-7: v1.0

---
+++ Lab6-7_store/VDemyanov.Shop/ShopWPF/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDemyanov.Shop.ShopWPF.ViewModels.Base
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            // [CallerMemberName] позволяет вызывать метод не указывая в параметрах имя свойства
+            // компилятор сам подставит имя метода из которого вызывается OnPropertyChanged
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        /// <summary>
+        /// Разрешает кольцевые обновления свойств
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field"></param>
+        /// <param name="value"></param>
+        /// <param name="PropertyName"></param>
+        /// <returns></returns>
+        protected virtual bool Set<T> (ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
---
+++ Lab6-7_store/VDemyanov.Shop/ShopWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,49 @@
+﻿using ShopWPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using VDemyanov.Shop.ShopWPF.Infrastructure.Commands;
+using VDemyanov.Shop.ShopWPF.ViewModels.Base;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace VDemyanov.Shop.ShopWPF.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        #region Заголовок окна
+        private string _Title = "Test";
+
+        /// <summary>Заголовок ока</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+
+        #region Команды
+
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            App.Current.Shutdown();
+        }
+
+        private bool CanCloseApplicationCommandExecuted(object p) => true;
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            #region Команды
+            CloseApplicationCommand = new RelayCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecuted);
+            #endregion
+        }
+    }
+}
