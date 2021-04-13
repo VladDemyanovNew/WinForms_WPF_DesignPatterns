55a3fc9a75d0ce8dc22de32337579c87e68e75e8
VDemyanov <vlad.demyanov.official@mail.ru>
2021-04-13 23:37:20 +0300

    Lab8: v2.0 (bad)

--- Lab6-7_store/VDemyanov.Shop/WPFCustomControls/CustomControl1.cs
+++ Lab6-7_store/VDemyanov.Shop/WPFCustomControls/CustomControl1.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 
-namespace VDemyanov.Shop.ShopWPF.Themes
+namespace VDemyanov.Shop.WPFCustomControls
 {
     /// <summary>
     /// Выполните шаги 1a или 1b, а затем 2, чтобы использовать этот пользовательский элемент управления в файле XAML.
@@ -15,33 +13,33 @@ namespace VDemyanov.Shop.ShopWPF.Themes
     /// Добавьте атрибут XmlNamespace в корневой элемент файла разметки, где он 
     /// будет использоваться:
     ///
-    ///     xmlns:MyNamespace="clr-namespace:VDemyanov.Shop.ShopWPF.Themes"
+    ///     xmlns:MyNamespace="clr-namespace:WPFCustomControls"
     ///
     ///
     /// Шаг 1б. Использование пользовательского элемента управления в файле XAML, существующем в другом проекте.
     /// Добавьте атрибут XmlNamespace в корневой элемент файла разметки, где он 
     /// будет использоваться:
     ///
-    ///     xmlns:MyNamespace="clr-namespace:VDemyanov.Shop.ShopWPF.Themes;assembly=VDemyanov.Shop.ShopWPF.Themes"
+    ///     xmlns:MyNamespace="clr-namespace:WPFCustomControls;assembly=WPFCustomControls"
     ///
     /// Потребуется также добавить ссылку из проекта, в котором находится файл XAML,
-    /// на данный проект и пересобрать во избежание ошибок компиляции:
+    /// на данный проект и заново выполнить построение во избежание ошибок компиляции:
     ///
     ///     Щелкните правой кнопкой мыши нужный проект в обозревателе решений и выберите
-    ///     "Добавить ссылку"->"Проекты"->[Поиск и выбор проекта]
+    ///     "Добавить ссылку"->"Проекты"->[Выберите этот проект]
     ///
     ///
     /// Шаг 2)
-    /// Теперь можно использовать элемент управления в файле XAML.
+    /// Продолжайте дальше и используйте элемент управления в файле XAML.
     ///
-    ///     <MyNamespace:Product/>
+    ///     <MyNamespace:CustomControl1/>
     ///
     /// </summary>
-    public class Product : Control
+    public class CustomCheckBox : ToggleButton
     {
-        static Product()
+        static CustomCheckBox()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(Product), new FrameworkPropertyMetadata(typeof(Product)));
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomCheckBox), new FrameworkPropertyMetadata(typeof(CustomCheckBox)));
         }
     }
 }
